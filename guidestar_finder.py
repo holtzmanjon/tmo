67a39d746659ccb1eb430c964072a5b2d656cbd0
Jon Holtzman <holtz@nmsu.edu>
2020-10-12 10:34:40 -0600

    initial commit

---
+++ guidestar_finder.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 18 14:26:47 2019
+
+Enter coords on the sky in console.
+It then displays an image of that part of the sky that is 1 deg diameter and 1500 pixels square
+It plots boxes for the main ccd and guide ccd that are the same apparent size as the 
+cameras on the Tortugas mountain observatory.
+The image is interactive, left mouse clicking a spot on the image will place the
+center of the main CCD over the curser position, and right mouse click will place
+the guide camera center. Both actions will move the other box respectivly.
+
+@author: Alexander
+"""
+
+from IPython import get_ipython
+#get_ipython().magic('reset -sf') 
+
+import argparse
+import wget
+import os
+import sys
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style
+plt.style.use(astropy_mpl_style)
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.coordinates import Angle
+from astropy import units
+from photutils import CircularAperture
+from photutils import RectangularAperture
+from astropy.coordinates import SkyCoord
+
+
+def coord_input():
+    '''Asks user to input coordinates in hr min sec/deg arcmin arcsec format
+    and converts those into degree decimals that the download link can use'''
+    
+    rastr = input('RA (hr:min:sec): ')
+    decstr = input("DEC (deg:min:sec): ")
+    return rastr, decstr
+
+def image_download(RA, DEC, fov = '1', Size = '750'):
+    """Generate URL and download image of sky centered around the coordinates specified
+    """
+    # uses user input to edit web address
+    #address contains all information about image, including location, resolution, FoV, etc
+    url = ('https://skyview.gsfc.nasa.gov/cgi-bin/images?Survey=digitized+sky+survey&position='+
+           RA+','+DEC+'&size='+fov+'&pixels='+Size+'&Return=FITS')
+
+    #Download image
+    wget.download(url, 'test.fits')
+    hdu = fits.open('test.fits')[0]
+    os.remove('test.fits')
+    return hdu
+    
+def image_setup(hdu):
+    """Displays input image and overlays coordinate grid pulled from WCS data
+    """
+    wcs = WCS(hdu.header)
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(1,1,1,projection = wcs)
+    ax.imshow(hdu.data, cmap = 'gray')
+    
+    return wcs, fig, ax
+
+def place_overlay(x_pixel, y_pixel, mousebutton, wcs, ax, FOV = '1', Size = '750'):
+    """Places either the main CCD, or the guide CCD apeture overlay 
+    centered on the pixel coordinates depending on the boolean case "guide"
+    specified in the input arguments. The overlay for the guide camera is placed
+    with respect to the CCD center. Function also replots image to reset apertures
+    (Easiest way I could find to do it for now)
+    """
+    fov = int(FOV)*60
+    size = int(Size)
+    scale = fov/size
+    #Set plate scale of image 60 is number of arcmin across 
+    
+    position_Target = (size/2, size/2)
+    aperture_Target = CircularAperture(position_Target, r=(0.65/scale))
+    aperture_Target.plot(color = 'blue', lw=1.5, alpha=0.5)
+    
+    if x_pixel == None or y_pixel == None:
+        position_CCD = (size/2, size/2)
+        position_Guide = (size/2-2/scale, ((size/2)-(16/scale)))
+    else:
+        if mousebutton:
+            position_Guide = (x_pixel, y_pixel)
+            position_CCD = ((x_pixel)+2/scale, (y_pixel+(16/scale)))
+        else:
+            position_Guide = (x_pixel-2/scale, y_pixel-(16/scale))    
+            position_CCD = ((x_pixel), (y_pixel))
+        
+    aperture_GuideStar = CircularAperture(position_Guide, r=(0.65/scale))
+    aperture_Guide = RectangularAperture(position_Guide, (6.14/scale),(4.85/scale))
+    aperture_GuideStar.plot(color = 'blue', lw=1.5, alpha=0.5)
+    aperture_Guide.plot(color = 'red', lw=1.5, alpha=0.5)
+    
+    
+    aperture_CCD = RectangularAperture(position_CCD,(18.4/scale),(13.8/scale))
+    aperture_CCD.plot(color = 'green', lw=1.5, alpha=0.5)
+    
+    ax.figure.canvas.draw()
+    
+    coords = SkyCoord.from_pixel(position_CCD[0], position_CCD[1], wcs)
+    RA_str = coords.ra.to_string(units.hour)
+    DEC_str = coords.dec.to_string(units.degree)
+
+    #print coords of main CCD center
+    print(RA_str, DEC_str)
+    
+def onclick(event, ax, wcs):
+    """Detects button press and reruns the overlay function to replot apertures
+    on the location of the click. checks if it was the left or right mouse button
+    Left mouse tells "place_overlay" to use mouse coords for Main CCD, and right mouse
+    click uses the mouse coords for the guide CCD
+    """
+    x = event.xdata
+    y = event.ydata
+    if event.button == 3:       #Checks if click was left or right mouse button
+        right = True
+    else:
+        right = False
+    
+    for p in reversed(ax.patches) : p.remove()
+    
+    place_overlay(x,y,right, wcs, ax)
+    
+def main(rastr=None,decstr=None):
+    """Main function to run all the parts
+    """
+
+    # get image from skyview
+    if rastr is None or decstr is None : 
+        ra,dec = coord_input()
+    ra=str(Angle(rastr+' h').deg)
+    dec=str(Angle(decstr+' d').deg)
+    print(ra,dec)
+    hdu = image_download(ra, dec)
+   
+    # display 
+    wcs, fig, ax = image_setup(hdu)
+   
+    # display views 
+    place_overlay(None, None, mousebutton = False, wcs = wcs, ax = ax)
+   
+    # start event loop for repointing 
+    fig.canvas.mpl_connect('button_press_event', lambda event: onclick(event, ax = ax, wcs = wcs))
+    
+    plt.show()
+    
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        prog=os.path.basename(sys.argv[0]),
+        description='Get adjusted coordinates to find guide star')
+
+    parser.add_argument("--ra", type=str, help='RA (J2000)')
+    parser.add_argument("--dec", type=str, help='DEC (J2000)')
+    args=parser.parse_args()
+
+    main(rastr=args.ra,decstr=args.dec)
+    
---
+++ rtml.py
@@ -0,0 +1,124 @@
+import pdb
+from astropy.io import ascii
+
+class Project() :
+
+    def __init__(self,user='holtz',email='holtz@nmsu.edu',organization='NMSU') :
+        self.user=user 
+        self.email=email
+        self.organization=organization
+
+    def open(self,file='test.xml') :
+        self.fout = open(file,'w')
+        self.fout.write('<?xml version="1.0" encoding="ISO-8859-1"?>\n')
+        self.fout.write('<RTML version="2.3">\n')
+        self.fout.write('<Contact><User>{:s}</User>\n'.format(self.user))
+        self.fout.write('<Email>{:s}</Email>\n'.format(self.email))
+        self.fout.write('<Organization>{:s}</Organization></Contact>\n'.format(self.organization))
+
+    def close(self) :
+        self.fout.write('</RTML>\n')
+        self.fout.close()
+
+    def write(self,request) :
+        self.fout.write('<Request>\n')
+        self.fout.write('<ID>{:s}</ID>\n'.format(request.targ))
+        self.fout.write('<UserName>{:s}</UserName>\n'.format(request.user))
+        self.fout.write('<Description>{:s}</Description>\n'.format(request.type))
+        self.fout.write('<Reason>monitor={:d}</Reason>\n'.format(request.monitor))
+        self.fout.write('<Project>{:s}</Project>\n'.format(request.project))
+        self.fout.write('<Schedule>\n')
+        self.fout.write('  <AirmassRange><Minimum>1</Minimum><Maximum>{:f}</Maximum></AirmassRange>\n'.format(request.airmax))
+        self.fout.write('  <SkyCondition>Good</SkyCondition>\n')
+        self.fout.write('  <Moon><Distance>20</Distance><Width>6</Width></Moon>\n')
+        self.fout.write('  <Priority>{:d}</Priority>\n'.format(request.priority))
+        self.fout.write('</Schedule>\n')
+        self.fout.write('<Target count="{:d}" interval="0" tolerance="0">\n'.format(request.repeat))
+        self.fout.write('  <Name>{:s}</Name>\n'.format(request.targ))
+        self.fout.write('  <Coordinates><RightAscension>{:f}</RightAscension><Declination>{:f}</Declination></Coordinates>\n'.format(request.ra,request.dec))
+        for exposure in request.exposures :
+            filter=exposure[0]
+            exptime=float(exposure[1])
+            nexp=int(exposure[2])
+            self.fout.write('  <Picture count="{:d}"><Name>{:s}</Name><Description> \#nopreview </Description>\n'.format(nexp,request.targ))
+            self.fout.write('    <ExposureTime>{:f}</ExposureTime><Binning>{:d}</Binning><Filter>{:s}</Filter>\n'.format(exptime,request.bin,filter))
+            self.fout.write('  </Picture>\n')
+        self.fout.write('</Target>\n')
+        self.fout.write('</Request>\n')
+
+
+class Request() :
+
+    def __init__(self,targ,user='NMSU',type='test',monitor=3,project='test',repeat=1,airmax=2,priority=5,ra=0,dec=0,bin=1,exposures=None) :
+        self.targ = targ
+        self.ra = ra
+        self.dec = dec
+        self.user = user
+        self.type = type
+        self.monitor = monitor
+        self.project = project
+        self.airmax = airmax
+        self.priority = priority
+        self.bin = bin
+        self.repeat = repeat
+        self.exposures = exposures
+
+def coords(rah,ram,ras,decd,decm,decs) :
+
+    ra = (int(rah)+int(ram)/60.+float(ras)/3600)*15
+    if '-' in decd:
+        dec = abs(int(decd))+int(decm)/60.+float(decs)/3600.
+        dec = -1 *dec
+    else :
+        dec = int(decd)+int(decm)/60.+float(decs)/3600.
+
+    return ra, dec
+
+def csv(file) :
+
+    out=Project()
+    out.open('test.rtml')
+
+    fp=open(file,'r')
+    for line in fp :
+        fields=line.split(',')
+        targ=fields[0]
+        rah,ram,ras=fields[1:4]
+        decd,decm,decs=fields[4:7]
+        ra,dec=coords(rah,ram,ras,decd,decm,decs)
+        type=fields[7]
+        priority=int(fields[8])
+        monitor=int(fields[9])
+        airmax= float(fields[10])
+        repeat=int(fields[11])
+        bin=int(fields[12])
+        nfilt=(len(fields)-13)//3
+        i=13
+        exposures=[]
+        for ifilt in range(nfilt) :
+            exposures.append([fields[i],fields[i+1],fields[i+2]])
+            i+=3
+        request=Request(targ,project=type,type=type,monitor=monitor,airmax=airmax,priority=priority,ra=ra,dec=dec,bin=bin,exposures=exposures,repeat=repeat)
+        out.write(request)
+
+    out.close()
+
+def catalog(file,user='test',priority=5,airmax=2,monitor=0,repeat=1,bin=1,type='test',project='test',exposures=[['V','300','1'],['I','300','1']]) :    
+
+    out=Project()
+    out.open(file+'.rtml')
+
+    dat=ascii.read(file,delimiter='|')
+    for line in dat :
+        targ=line['col2']
+        rah,ram,ras=line['col3'].split()
+        decd,decm,decs=line['col4'].split()
+        ra,dec=coords(rah,ram,ras,decd,decm,decs)
+
+        request=Request(targ,user=user,project=project,type=type,monitor=monitor,airmax=airmax,priority=priority,ra=ra,dec=dec,bin=bin,exposures=exposures)
+        out.write(request)
+
+    out.close()
+
+catalog('Messier.txt',user='NMSU',project='Messier',type='Messier catalog',exposures=[['B','300','1'],['V','300','1'],['R','300','1']])
+#csv('test.csv') 
