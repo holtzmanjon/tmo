3b82ac11a84c454af315fd60d4b97612adf8e490
Jon Holtzman <holtz@nmsu.edu>
2021-10-22 13:41:25 -0600

    comment, add bestefforts, fix description

--- rtml.py
+++ rtml.py
@@ -2,13 +2,18 @@ import pdb
 from astropy.io import ascii
 
 class Project() :
-
+    """ class for an RTML Project
+    """
     def __init__(self,user='holtz',email='holtz@nmsu.edu',organization='NMSU') :
+        """ Initialize with user information
+        """
         self.user=user 
         self.email=email
         self.organization=organization
 
     def open(self,file='test.xml') :
+        """ Open RTML file for output with basic header
+        """
         self.fout = open(file,'w')
         self.fout.write('<?xml version="1.0" encoding="ISO-8859-1"?>\n')
         self.fout.write('<RTML version="2.3">\n')
@@ -17,11 +22,15 @@ class Project() :
         self.fout.write('<Organization>{:s}</Organization></Contact>\n'.format(self.organization))
 
     def close(self) :
+        """ Close RTML file
+        """
         self.fout.write('</RTML>\n')
         self.fout.close()
 
     def write(self,request) :
-        self.fout.write('<Request>\n')
+        """ Write a target Request to RTML
+        """
+        self.fout.write('<Request bestefforts="True">\n')
         self.fout.write('<ID>{:s}</ID>\n'.format(request.targ))
         self.fout.write('<UserName>{:s}</UserName>\n'.format(request.user))
         self.fout.write('<Description>{:s}</Description>\n'.format(request.type))
@@ -40,7 +49,7 @@ class Project() :
             filter=exposure[0]
             exptime=float(exposure[1])
             nexp=int(exposure[2])
-            self.fout.write('  <Picture count="{:d}"><Name>{:s}</Name><Description> \#nopreview </Description>\n'.format(nexp,request.targ))
+            self.fout.write('  <Picture count="{:d}"><Name>{:s}</Name><Description>#nopreview </Description>\n'.format(nexp,request.targ))
             self.fout.write('    <ExposureTime>{:f}</ExposureTime><Binning>{:d}</Binning><Filter>{:s}</Filter>\n'.format(exptime,request.bin,filter))
             self.fout.write('  </Picture>\n')
         self.fout.write('</Target>\n')
@@ -48,6 +57,8 @@ class Project() :
 
 
 class Request() :
+    """ Class to hold target request information
+    """
 
     def __init__(self,targ,user='NMSU',type='test',monitor=3,project='test',repeat=1,airmax=2,priority=5,ra=0,dec=0,bin=1,exposures=None) :
         self.targ = targ
@@ -64,7 +75,8 @@ class Request() :
         self.exposures = exposures
 
 def coords(rah,ram,ras,decd,decm,decs) :
-
+    """ routine to convert hms/dms to decimal
+    """
     ra = (int(rah)+int(ram)/60.+float(ras)/3600)*15
     if '-' in decd:
         dec = abs(int(decd))+int(decm)/60.+float(decs)/3600.
@@ -75,11 +87,14 @@ def coords(rah,ram,ras,decd,decm,decs) :
     return ra, dec
 
 def csv(file,user='NMSU',project='test') :
-
+    """ Create RTML from a CSV file with individual targets and request configurations
+    """
     out=Project()
     out.open(file+'.rtml')
 
     fp=open(file,'r')
+    # input CSV format:
+    # target,rah,ram,ras,decd,decm,decs,type,priority,monitor,airmax,repeat,bin,{nfilt}*[filter,exptime,count]
     for line in fp :
         fields=line.split(',')
         targ=fields[0]
@@ -104,6 +119,8 @@ def csv(file,user='NMSU',project='test') :
     out.close()
 
 def catalog(file,user='test',priority=5,airmax=2,monitor=0,repeat=1,bin=1,type='test',project='test',exposures=[['V','300','1'],['I','300','1']]) :    
+    """ Create RTML from a catalog of targets, all with the same request configuration
+    """
 
     out=Project()
     out.open(file+'.rtml')
